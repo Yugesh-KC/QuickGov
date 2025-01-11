1cd92ba442e025b852cc82122f9dca53c1b9d646
Yugesh-KC <yugeskc10@gmail.com>
2025-01-11 14:57:39 +0545

    pdf handling

--- image_to_english.py
+++
@@ -1,8 +0,0 @@
-from ocr import ocr_from_image
-from translate import translate_to_english
-
-def image_to_english(image_path,api_key):
-    nepali_text=ocr_from_image(image_path)
-    english_text=translate_to_english(nepali_text,api_key)
-    return english_text
-
